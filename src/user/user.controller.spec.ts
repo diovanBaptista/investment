4e58ae70af20c5961d6f884e8353f41d22c91d71
Daniel Damasio <damasio.dan7@gmail.com>
2024-02-29 11:20:56 -0300

    test: test typeorm conection and define controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,7 +11,13 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
