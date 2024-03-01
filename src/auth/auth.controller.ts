cf0a0d4f760a29ecd90f4e0fa9e6b3922f043e85
Daniel Damasio <damasio.dan7@gmail.com>
2024-03-01 10:23:29 -0300

    docs: add route documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -9,6 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Authenticate user login' })
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
