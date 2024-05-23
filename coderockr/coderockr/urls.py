4b84d31c77bde2914eae37c5e6eee7d054bf3838
Diovan <diovantrab@gamil.com>
2024-05-22 23:22:00 -0300

    cleaning coidgo

--- coderockr/coderockr/urls.py
+++ coderockr/coderockr/urls.py
@@ -5,11 +5,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-# from rest_framework_simplejwt.authentication import JWTAuthentication
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,18 +19,14 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    # authentication_classes=(JWTAuthentication,),
     )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('hello/' , hello_world, name= 'hello-world' ), 
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('investment.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken')),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- coderockr/investment/urls.py
+++ coderockr/investment/urls.py
@@ -1,10 +1,11 @@
-from os import name
 from django.urls import path, include
 from rest_framework import routers
 from .viewsets import InvestimentationViewSet, WithdrawViewSet, InvestorViewSet
 from .viewsets import WithdrawViewSet
 
 api_investment = routers.DefaultRouter()
+api_investor = routers.DefaultRouter()
+api_withdraw = routers.DefaultRouter()
 
 api_investment.register(
     'investment',
@@ -12,13 +13,13 @@ api_investment.register(
     basename='investment'
 )
 
-api_investment.register(
+api_withdraw.register(
     'withdraw',
     WithdrawViewSet,
     basename='withdraw'
 )
 
-api_investment.register(
+api_investor.register(
     'investor',
     InvestorViewSet,
     basename='investor'
@@ -26,4 +27,6 @@ api_investment.register(
 
 urlpatterns = [
     path('investment/', include(api_investment.urls)),
+    path('withdraw/', include(api_withdraw.urls)),
+    path('investor/', include(api_investor.urls)),
 ]
